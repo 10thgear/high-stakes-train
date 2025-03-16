c15fda789205ffd869019e5d4eb6234cb316a07c
Jun Ye <yetsun@gmail.com>
2025-03-15 22:58:07 -0700

    fix linter

--- yolotest.py
+++ yolotest.py
@@ -1,13 +1,15 @@
 from ultralytics import YOLO
 
-model =  YOLO('runs\\detect\\train7\\weights\\best.pt')
+model = YOLO("runs\\detect\\train7\\weights\\best.pt")
 
-#model.predict('E:\\work\\highstakes\\test_2.jpg', save = True)
-#model.predict('E:\\work\\highstakes\\test_3.jpg', save = True)
-#model.predict('E:\\work\\highstakes\\test_4.jpg', save = True)
+# model.predict('E:\\work\\highstakes\\test_2.jpg', save = True)
+# model.predict('E:\\work\\highstakes\\test_3.jpg', save = True)
+# model.predict('E:\\work\\highstakes\\test_4.jpg', save = True)
 
 
-model.predict('test_data\\videoplayback_4.mp4', save = True, hide_labels=True, hide_conf=True)
+model.predict(
+    "test_data\\videoplayback_4.mp4", save = True, hide_labels=True, hide_conf=True
+)
 
-#model.predict('E:\\work\\highstakes\\maxresdefault.jpg', save = True, hide_labels=True, hide_conf=True)
-#model.predict('E:\\work\\highstakes\\training_data_detect_ring\\valid\\images\\image_jpg.rf.b373a82ad9b08e583772f0b505beacf2.jpg', save = True)
+# model.predict('E:\\work\\highstakes\\maxresdefault.jpg', save = True, hide_labels=True, hide_conf=True)
+# model.predict('E:\\work\\highstakes\\training_data_detect_ring\\valid\\images\\image_jpg.rf.b373a82ad9b08e583772f0b505beacf2.jpg', save = True)
--- yolotrain.py
+++ yolotrain.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
 
-model = YOLO('yolov8n.pt')
+model = YOLO("yolov8n.pt")
 
-if __name__ == '__main__':
-    model.train(data = 'highstakes.yaml', epochs = 100)
-    #model.train(data = 'safehat.yaml', epochs = 10, device = 'cpu')
+if __name__ == "__main__":
+    model.train(data="highstakes.yaml", epochs = 100)
+    # model.train(data = 'safehat.yaml', epochs = 10, device = 'cpu')
     model.val()
